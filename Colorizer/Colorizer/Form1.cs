cc95ec146892e81510812292b45dfbd841a16ad7
ArmenAg <armen.ag@live.com>
2014-11-04 15:58:29 -0800

    Basic Learning

--- Colorizer/Colorizer/Form1.cs
+++ Colorizer/Colorizer/Form1.cs
@@ -1,8 +1,11 @@
-﻿using Colorizer.Imaging;
+﻿using Accord.Neuro.Learning;
+using Accord.Neuro.Networks;
+using Colorizer.Imaging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,12 +19,40 @@ namespace Colorizer
         public Form1()
         {
             InitializeComponent();
-            
+
             var bit = new LockBitmap((Bitmap)Bitmap.FromFile(@"C:\Users\armen_000\Pictures\heic1404b1920.jpg"));
             Imaging.LockBitmapTools.InPlaceGrayScale(bit);
             this.mainPictureBox.Image = bit;
 
-            bit.SetPixel(0, 0, Color.FloralWhite);
+            DeepBeliefNetwork network = new Accord.Neuro.Networks.DeepBeliefNetwork(2, 23, 1);
+            LevenbergMarquardtLearning learn = new Accord.Neuro.Learning.LevenbergMarquardtLearning(network);
+
+            double[][] input = new double[][]
+            {
+                new double[]{0,0},
+                new double[]{0,1},
+                new double[]{1,0},
+                new double[]{1,1}
+            };
+
+            double[][] output = new double[][]
+            {
+                new double[]{0},
+                new double[]{1},
+                new double[]{1},
+                new double[]{0}
+            };
+            for (int i = 0; i < 100; i++)
+            {
+                learn.RunEpoch(input, output);                
+            }
+
+            Debug.WriteLine(network.Compute(new double[] { 0, 0 }).First());
+            Debug.WriteLine(network.Compute(new double[] { 1, 0 }).First());
+            Debug.WriteLine(network.Compute(new double[] { 0, 1 }).First());
+            Debug.WriteLine(network.Compute(new double[] { 1, 1 }).First());
+
+            Debugger.Break();
         }
     }
 }
