6e63d40ec9ad45a408917515b5ed7306322e01c1
Tarik Onalan <phi.quantum@gmail.com>
2014-11-08 23:31:27 -0800

    ...

--- Colorizer/Colorizer/Form1.cs
+++ Colorizer/Colorizer/Form1.cs
@@ -22,41 +22,9 @@ namespace Colorizer
         public Form1()
         {
             InitializeComponent();
-            this.TestLearningTry();
+            this.LearnPhoto();
         }
-
-        private static void TestLearning()
-        {
-            DeepBeliefNetwork network = new Accord.Neuro.Networks.DeepBeliefNetwork(2, 23, 1);
-            LevenbergMarquardtLearning learn = new Accord.Neuro.Learning.LevenbergMarquardtLearning(network);
-
-            double[][] input = new double[][]
-            {
-                new double[]{0,0},
-                new double[]{0,1},
-                new double[]{1,0},
-                new double[]{1,1}
-            };
-
-            double[][] output = new double[][]
-            {
-                new double[]{0},
-                new double[]{1},
-                new double[]{1},
-                new double[]{0}
-            };
-
-            for (int i = 0; i < 100; i++)
-            {
-                learn.RunEpoch(input, output);
-            }
-
-            Debug.WriteLine(network.Compute(new double[] { 0, 0 }).First());
-            Debug.WriteLine(network.Compute(new double[] { 1, 0 }).First());
-            Debug.WriteLine(network.Compute(new double[] { 0, 1 }).First());
-            Debug.WriteLine(network.Compute(new double[] { 1, 1 }).First());
-        }
-        private void TestLearningTry()
+        private void LearnPhoto()
         {
             var path = System.Environment.GetEnvironmentVariable("USERPROFILE");
             var bit = new LockBitmap((Bitmap)Bitmap.FromFile(path + "\\Pictures\\butterfly.jpg"));
@@ -71,7 +39,7 @@ namespace Colorizer
             DeepBeliefNetwork network = new DeepBeliefNetwork(9, 23, 3);
             new NguyenWidrow(network).Randomize();
             ParallelResilientBackpropagationLearning learning = new ParallelResilientBackpropagationLearning(network);
-            for (int i = 0; i < 250; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(learning.RunEpoch(input, output));
             }
