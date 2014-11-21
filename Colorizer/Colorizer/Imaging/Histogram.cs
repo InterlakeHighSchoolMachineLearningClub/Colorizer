3ecb26ca4cf2e0d14c46318eadeb1e1687c4e39d
ArmenAg <armen.ag@live.com>
2014-11-20 16:03:32 -0800

    ?

---
+++ Colorizer/Colorizer/Imaging/Histogram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colorizer.Imaging
+{
+    class Histogram
+    {   
+        //public int[] imagePixels;
+        public Histogram (int numBins, LockBitmap bitmap) 
+        {
+            int[] ImagePixels = new int[numBins];
+
+            ImageArray(bitmap);
+        }
+
+        public void ImageArray(LockBitmap bitmap, int[] ImagePixels)
+        {
+            for(int j = 0; j < ImagePixels.Length; j++)
+            {
+                ImagePixels[j] = bitmap[]
+            }
+        }
+
+        public void sortPixelData(int[] ImagePixels)
+        {
+            foreach(int pixel in ImagePixels)
+            {
+                
+            }
+        }
+
+        public void displayOutput()
+        {
+
+        }
+        //constructor, with number of bins, image file data
+        //method to get array of all pixel values
+        //method to create bins, sort pixel data array 
+        //method to output just the # of values per bin
+    }
+}
