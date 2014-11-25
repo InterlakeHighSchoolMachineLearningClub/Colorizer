f0969df5f0a3cda77e8c4d9b2e06a7012fb81e5d
ArmenAg <armen.ag@live.com>
2014-11-25 15:26:24 -0800

    histogram refactoring

---
+++ Colorizer/Colorizer/Imaging/ColorHistogram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colorizer.Imaging
+{
+    class ColorHistogram
+    {
+        public readonly int NumberOfBins;
+        public readonly LockBitmap Bitmap;
+
+        private readonly byte[] R;
+        private readonly byte[] G;
+        private readonly byte[] B;
+
+        public ColorHistogram(double perPixel, LockBitmap bitmap)
+        {
+            this.NumberOfBins = (int)((255D / perPixel) + 1);
+
+            this.Bitmap = bitmap;
+
+            this.R = new byte[this.NumberOfBins];
+            this.G = new byte[this.NumberOfBins];
+            this.B = new byte[this.NumberOfBins];
+
+        }
+
+    }
+}
