14d42d527ed6c079d1acff1484a03cf30468b22d
ArmenAg <armen.ag@live.com>
2014-11-25 16:00:18 -0800

    histogram

--- Colorizer/Colorizer/Imaging/ColorHistogram.cs
+++ Colorizer/Colorizer/Imaging/ColorHistogram.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +11,37 @@ namespace Colorizer.Imaging
     class ColorHistogram
     {
         public readonly int NumberOfBins;
+
+        public readonly double PerPixel;
+
         public readonly LockBitmap Bitmap;
 
-        private readonly byte[] R;
-        private readonly byte[] G;
-        private readonly byte[] B;
+        private readonly int[] R;
+        private readonly int[] G;
+        private readonly int[] B;
 
         public ColorHistogram(double perPixel, LockBitmap bitmap)
         {
-            this.NumberOfBins = (int)((255D / perPixel) + 1);
+            Trace.Assert(perPixel > 1);
 
+            this.NumberOfBins = (int)((255D / perPixel);
+            this.PerPixel = perPixel;
             this.Bitmap = bitmap;
 
-            this.R = new byte[this.NumberOfBins];
-            this.G = new byte[this.NumberOfBins];
-            this.B = new byte[this.NumberOfBins];
+            this.R = new int[this.NumberOfBins];
+            this.G = new int[this.NumberOfBins];
+            this.B = new int[this.NumberOfBins];
 
+            foreach (Color i in bitmap.GetAllColors())
+            {
+                this.R[this.GetPosition(i.R)]++;
+                this.G[this.GetPosition(i.G)]++;
+                this.B[this.GetPosition(i.B)]++;
+            }
         }
-
+        private int GetPosition(byte b)
+        {
+            return (int)((double)(b) / this.PerPixel);
+        }
     }
 }
